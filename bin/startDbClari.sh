5df9a4fe3aba32909d0d016db96909bd7357023c
santosbogo <sbogo@mail.austral.edu.ar>
2024-04-25 19:52:49 -0300

    DB start modifications

--- bin/startDbClari.sh
+++ bin/startDbClari.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-java -cp /Users/claralopez/IdeaProjects/hsqldb-2.7.2/hsqldb/lib/hsqldb.jar org.hsqldb.server.Server --database.0 file:../db/AccessControlDB--dbname.0 AccessControlDB
+java -cp /Users/claralopez/IdeaProjects/hsqldb-2.7.2/hsqldb/lib/hsqldb.jar org.hsqldb.server.Server --database.0 file:/Users/claralopez/IdeaProjects/hsqldb-2.7.2/hsqldb/db/accessControl --dbname.0 AccessControlDB
