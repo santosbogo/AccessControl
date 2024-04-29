bd0c41eabecf23768532669a4f6c37b7e73c62e3
luzlaura <lucitalaura@gmail.com>
2024-04-29 11:42:03 -0300

    App.js

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SignUp from './Components/GetStarted/SignUp';
+import Login from './Components/GetStarted/Login';
+
 
 
 function App() {
@@ -8,6 +10,7 @@ function App() {
         <Router>
             <Routes>
                 <Route path="/signup" element={<SignUp />} />
+                <Route path="/login" element={<Login/>}/>
             </Routes>
         </Router>
     );
