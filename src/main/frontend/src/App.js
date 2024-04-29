341bc4c40c2d49c60257988d5d05b533cc78edd5
ClaraLopez1 <mclopez@mail.austral.edu.ar>
2024-04-29 11:10:09 -0300

    front, home/manageUsers/History

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SignUp from './Components/GetStarted/SignUp';
-import Home from './Components/Home/Home';
-import ManageUsers from './Components/Home/ManageUsers';
-import History from './Components/Home/History';
+
 
 function App() {
     return (
         <Router>
             <Routes>
-                <Route path="/signup" element={<SignUp/>}/>
-                <Route path="/Home" element={<Home/>}/>
-                <Route path="/Home/manage-users" element={<ManageUsers/>}/>
-                <Route path="/Home/view-history" element={<History/>}/>
+                <Route path="/signup" element={<SignUp />} />
             </Routes>
         </Router>
     );
 }
 
 export default App;
-
-
-
