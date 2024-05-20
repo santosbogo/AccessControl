19d02dd0fb7fdcab4dc076cdf846563fbdfc08f2
inesgassiebayle <igassiebayle@mail.austral.edu.ar>
2024-05-20 10:02:16 -0300

    changes in persistence to connect to the db from application

--- src/main/java/accesscontrol/persistence/Starter.java
+++ src/main/java/accesscontrol/persistence/Starter.java
@@ -4,5 +4,7 @@ public class Starter {
     public static void main(String[] args) {
         Database database = new Database();
         database.startDBServer();
+
+        database.stopDBServer();
     }
 }
