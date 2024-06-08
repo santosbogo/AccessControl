53932bd220ab325ed514db4e0db45fbbf1ac6f7c
inesgassiebayle <igassiebayle@mail.austral.edu.ar>
2024-06-08 16:12:47 -0300

    changes for new db mysql connection

--- bin/startDbLuz.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-java -cp /Users/luzlaura/IdeaProjects/elecinf/hsqldb-2.7.2/hsqldb/lib/hsqldb.jar  org.hsqldb.server.Server --database.0 file:/Users/luzlaura/IdeaProjects/elecinf/hsqldb-2.7.2/hsqldb/db/accessControl --dbname.0 accessControlDB
-
