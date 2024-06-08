58725f8d8d169133d45ddb843437a4bed836f80a
Santos Bogo <72747461+santosbogo@users.noreply.github.com>
2024-06-08 19:11:47 -0300

    Trying change ip from a file

--- src/main/frontend/src/Components/GetStarted/Login.jsx
+++ src/main/frontend/src/Components/GetStarted/Login.jsx
@@ -11,7 +11,7 @@ const Login = () => {
 
     const login = async () => {
         try {
-            const response = await axios.post(`http://54.145.6.143:3333/admin/login`, {
+            const response = await axios.post(`http://${process.env.PUBLIC_IP}/admin/login`, {
                 username: username,
                 password: password
             });
